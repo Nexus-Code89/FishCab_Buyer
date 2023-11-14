340e9b39fcc1b4eff1cd8c4a9f0d045323805ec5
Yureen Flores <yureenflores@gmail.com>
2023-11-14 11:23:59 +0800

    added register page and functionality

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fish_cab/pages/login_or_register_page.dart';
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'login_page.dart';
@@ -13,15 +14,15 @@ class AuthPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           // user is logged in
-          if (snapshot. hasData) {
+          if (snapshot.hasData) {
             return HomePage();
           }
           // user is NOT logged in
           else {
-            return LoginPage();
+            return LoginOrRegisterPage();
           }
         },
-       ),
+      ),
     );
   }
 }
