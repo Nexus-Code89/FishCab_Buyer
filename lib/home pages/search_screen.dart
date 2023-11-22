777d3ef8e20ba3cbc71d3046082a906506cfa8f5
DEP User <depuser@DEPs-MacBook-Air.local>
2023-11-22 14:30:41 +0800

    search seller (partial)

--- lib/home pages/search_screen.dart
+++ lib/home pages/search_screen.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:fish_cab/home pages/bottom_navigation_bar.dart';
+import 'package:fish_cab/home%20pages/bottom_navigation_bar.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -10,35 +11,125 @@ class _SearchScreenState extends State<SearchScreen> with AutomaticKeepAliveClie
   @override
   bool get wantKeepAlive => true;
 
+  // Add any necessary state variables here
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text('Search Screen'),
       ),
-      body: Center(
-        child: Text('This is the Search Screen!'),
-      ),
+      body: SearchView(), // Extracted into a separate stateful widget
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: 1,
         onTap: (index) {
           switch (index) {
-          case 0:
-            // Navigate to HomeScreen
-            Navigator.pushReplacementNamed(context, '/home');
-            break;
-          case 2:
-            // Navigate to ChatsScreen
-            Navigator.pushReplacementNamed(context, '/chats');
-            break;
-          case 3:
-            // Navigate to NotificationsScreen
-            Navigator.pushReplacementNamed(context, '/notifications');
-            break;
+            case 0:
+              // Navigate to HomeScreen
+              Navigator.pushReplacementNamed(context, '/home');
+              break;
+            case 2:
+              // Navigate to ChatsScreen
+              Navigator.pushReplacementNamed(context, '/chats');
+              break;
+            case 3:
+              // Navigate to NotificationsScreen
+              Navigator.pushReplacementNamed(context, '/notifications');
+              break;
           }
         },
       ),
+    );
+  }
+}
+
+class SearchView extends StatefulWidget {
+  SearchView({super.key});
+
+  @override
+  State<SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<SearchView> {
+  var searchName = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: SizedBox(
+          height: 40,
+          child: TextField(
+            onChanged: (value) { // fetch string from searchbar
+              setState(() {
+                searchName = value;
+              });
+            },
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Search',
+                hintStyle: TextStyle(color: Colors.grey),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                  focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                ),
+          ),
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .orderBy('firstName')
+              .startAt([searchName]).endAt([searchName + "\uf8ff"]).snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var data = snapshot.data!.docs[index];
+                  return ListTile(
+                    // TODO: create the seller's ProfileView to be rendered when the profile_info is tapped.
+                    // YT VID REFERENCE LINK: https://youtu.be/z0x1bIkn6Do?si=hBLbBQS5d9yYlTz6
+                    /*onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileView(
+                                  userId: data['id'],
+                                )),
+                      );
+                    },*/
+                    leading: CircleAvatar(
+                      radius: 24,
+                      //backgroundImage: NetworkImage(data['profileUrl']),
+                    ),
+                    title: Text(data['firstName']),
+                    subtitle: Text(data['email']),
+                  );
+                });
+          }),
     );
   }
 }
