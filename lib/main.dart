bd8e2fd392d773011f6fd9aa755d5f6ffbcecf05
Yureen Flores <yureenflores@gmail.com>
2023-12-01 16:21:33 +0800

    chat mvc attempt

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:fish_cab/seller_pages/seller_search_singleton.dart';
 import 'package:fish_cab/seller_side/seller_fish_options_page.dart';
 import 'package:fish_cab/seller_side/seller_home_page.dart';
 import 'package:fish_cab/seller_side/seller_schedule_page.dart';
-import 'package:fish_cab/seller_side/seller_schedule_update.dart';
 import 'package:fish_cab/seller_side/seller_singleton.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -38,14 +37,21 @@ class MyApp extends StatelessWidget {
           '/search': (context) => SearchScreen(),
           '/chats': (context) => ChatsScreen(),
           '/notifications': (context) => NotificationsScreen(),
-          '/seller_home_view':(context) => SellerHomeScreen(userId: SellerSeacrhSingleton.instance.userId,),
-          '/seller_schedule_view':(context) => SellerScheduleScreen(sellerId: SellerSeacrhSingleton.instance.userId,),
-          '/seller_fish_options_view':(context) => FishOptionsScreen(sellerId: SellerSeacrhSingleton.instance.userId,),
-          '/seller_home':(context) => SellerHomePage(),
-          '/seller_schedule':(context) => SellerSchedulePage(sellerId: SellerSingleton.instance.userId,),
-          '/seller_fish_options':(context) => FishOptionsPage(),
+          '/seller_home_view': (context) => SellerHomeScreen(
+                userId: SellerSeacrhSingleton.instance.userId,
+              ),
+          '/seller_schedule_view': (context) => SellerScheduleScreen(
+                sellerId: SellerSeacrhSingleton.instance.userId,
+              ),
+          '/seller_fish_options_view': (context) => FishOptionsScreen(
+                sellerId: SellerSeacrhSingleton.instance.userId,
+              ),
+          '/seller_home': (context) => SellerHomePage(),
+          '/seller_schedule': (context) => SellerSchedulePage(
+                sellerId: SellerSingleton.instance.userId,
+              ),
+          '/seller_fish_options': (context) => FishOptionsPage(),
 //'/schedule_update': (context) => ScheduleUpdatePage(),
-        }
-    );
+        });
   }
 }
