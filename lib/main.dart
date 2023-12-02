e622f719d8c2ef5fb525245a71ba6dc07aa07893
DEP User <depuser@DEPs-MacBook-Air.local>
2023-12-02 12:19:18 +0800

    fish options implemented

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:fish_cab/seller_pages/seller_fish_options_screen.dart';
 import 'package:fish_cab/seller_pages/seller_home_screen.dart';
 import 'package:fish_cab/seller_pages/seller_schedule_screen.dart';
 import 'package:fish_cab/seller_pages/seller_search_singleton.dart';
+import 'package:fish_cab/seller_side/seller_add_fish_option.dart';
 import 'package:fish_cab/seller_side/seller_fish_options_page.dart';
 import 'package:fish_cab/seller_side/seller_home_page.dart';
 import 'package:fish_cab/seller_side/seller_schedule_page.dart';
@@ -32,20 +33,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/make_review', // Set the initial route
+        initialRoute: '/auth', // Set the initial route
         routes: {
           '/auth': (context) => const AuthPage(),
           '/home': (context) => HomePage(),
           '/search': (context) => SearchScreen(),
           '/chats': (context) => ChatsScreen(),
           '/notifications': (context) => NotificationsScreen(),
-          '/seller_home': (context) => SellerHomeScreen(
-                userId: SellerSingleton.instance.userId,
-              ),
-          '/seller_schedule': (context) => SellerScheduleScreen(
-                sellerId: SellerSingleton.instance.userId,
-              ),
-          '/seller_fish_options': (context) => FishOptionsScreen(
+          '/seller_home_view':(context) => SellerHomeScreen(userId: SellerSeacrhSingleton.instance.userId,),
+          '/seller_schedule_view':(context) => SellerScheduleScreen(sellerId: SellerSeacrhSingleton.instance.userId,),
+          '/seller_fish_options_view':(context) => FishOptionsScreen(sellerId: SellerSeacrhSingleton.instance.userId,),
+          '/seller_home':(context) => SellerHomePage(),
+          '/seller_fish_options':(context) => FishOptionsPage(sellerId: SellerSingleton.instance.userId,),
+          '/add_fish_option':(context) => AddFishOptionPage(sellerId: SellerSingleton.instance.userId,),
+          '/seller_schedule': (context) => SellerSchedulePage(
                 sellerId: SellerSingleton.instance.userId,
               ),
           '/make_review': (context) => ReviewView(
